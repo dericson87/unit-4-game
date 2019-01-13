53f8a13a6f00c1ea57790c8d9b4a270f711e963e
Derek Ericson <derek.ericson@gmail.com>
2019-01-12 18:38:15 -0800

    cleaned up code to be more DRY

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,65 @@
-$( document ).ready(function(){
-    var Random=Math.floor(Math.random()*101+19)
-   
-    $('#randomNumber').text(Random);
-   
-    var num1= Math.floor(Math.random()*11+1)
-    var num2= Math.floor(Math.random()*11+1)
-    var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
-   
-    var userTotal= 0; 
-    var wins= 0;
-    var losses = 0;
+$(document).ready(function () {
   
+  var goalNum = 0;
+
+  var num1 = 0;
+  var num2 = 0;
+  var num3 = 0;
+  var num4 = 0;
+
+  var userTotal = 0;
+  var wins = 0;
+  var losses = 0;
+
   $('#numberWins').text(wins);
   $('#numberLosses').text(losses);
 
-  function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        $('#randomNumber').text(Random);
-        num1= Math.floor(Math.random()*11+1);
-        num2= Math.floor(Math.random()*11+1);
-        num3= Math.floor(Math.random()*11+1);
-        num4= Math.floor(Math.random()*11+1);
-        userTotal= 0;
-        $('#finalTotal').text(userTotal);
-        } 
+  function reset() {
+    goalNum = Math.floor(Math.random() * 101 + 19);
+    $('#randomNumber').text(goalNum);
+    num1 = Math.floor(Math.random() * 11 + 1);
+    num2 = Math.floor(Math.random() * 11 + 1);
+    num3 = Math.floor(Math.random() * 11 + 1);
+    num4 = Math.floor(Math.random() * 11 + 1);
+    userTotal = 0;
+    $('#finalTotal').text(userTotal);
+  }
 
-  function winner(){
-    $("#winLossAlert").html( "<p>You win!</p>");
-    wins++; 
+  function winner() {
+    $("#winLossAlert").html("<p>You win!</p>");
+    wins++;
     $('#numberWins').text(wins);
     reset();
   }
- 
-  function loser(){
-    $("#winLossAlert").html( "<p>You lose!</p>" );
+
+  function loser() {
+    $("#winLossAlert").html("<p>You lose!</p>");
     losses++;
     $('#numberLosses').text(losses);
     reset()
   }
- 
-    $('#one').on ('click', function(){
-      userTotal = userTotal + num1;
-      $('#finalTotal').text(userTotal); 
-          if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }   
-    })  
-    $('#two').on ('click', function(){
-      userTotal = userTotal + num2;
-  
-      $('#finalTotal').text(userTotal); 
-          if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#three').on ('click', function(){
-      userTotal = userTotal + num3;
-     
-      $('#finalTotal').text(userTotal);
- 
-            if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#four').on ('click', function(){
-      userTotal = userTotal + num4;
-      $('#finalTotal').text(userTotal);   
-            if (userTotal == Random){
-            winner();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }
-    });   
-  }); 
+
+  $('#one, #two, #three, #four').on('click', function (event) {
+    console.log(event.target)
+    var num = -1;
+    if(event.target.id == 'one')
+      num = num1;
+    if(event.target.id == 'two')
+      num = num2;
+      if(event.target.id == 'three')
+      num = num3;
+      if(event.target.id == 'four')
+      num = num4;
+      console.log("Adding: " + num)
+    userTotal = userTotal + num;
+    $('#finalTotal').text(userTotal);
+    if (userTotal == goalNum) {
+      winner();
+    } else if (userTotal > goalNum) {
+      loser();
+    }
+  })
+
+    
+  reset();
+});
